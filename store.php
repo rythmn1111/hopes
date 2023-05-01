c80ed72b24ce012539b86450acf1029c7d7fe518
rythmn1111 <82413584+rythmn1111@users.noreply.github.com>
2023-05-02 03:25:34 +0530

    Update store.php

--- store.php
+++ store.php
@@ -1,23 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "message_board";
+$file = 'messages.xml';
 
 // Get the message from the request body
 $message = json_decode(file_get_contents('php://input'))->message;
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Load the existing messages from the XML file
+$xml = simplexml_load_file($file);
+if (!$xml) {
+  $xml = new SimpleXMLElement("<messages></messages>");
 }
 
-// Insert the new message into the database
-$sql = "INSERT INTO messages (message) VALUES ('" . $conn->real_escape_string($message) . "')";
-if ($conn->query($sql) === TRUE) {
-  http_response_code(201);
-} else {
-  http_response_code
+// Add the new message to the XML file
+$entry = $xml->addChild('message', htmlspecialchars($message));
+$entry->addAttribute('timestamp', time());
+file_put_contents($file, $xml->asXML());
+http_response_code(201);
